65dee1d0ae7b5eecb21a18953bceb0d9865b8ebb
Yong gui rong <121320598@qq.com>
2017-11-16 20:31:40 +0800

    Create common.php

---
+++ common/common.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * 自动加载文件
+ * 
+ */
+function autoLoad($currPath) {
+	if (is_dir($currPath)) {
+		$handler = opendir ($currPath);
+		while (($filename = readdir( $handler )) !== false) {
+			if ($filename != "." && $filename != ".." && $filename[0] != '.') {		
+				if(is_file($currPath . '/' . $filename)) {		
+					require_once $currPath . '/' . $filename;
+				}
+				if(is_dir($currPath . '/' . $filename)) {		
+					autoLoad($currPath . '/' . $filename);
+				}
+			}
+		}
+		closedir($handler);
+	}
+}
+
+autoLoad(dirname(__FILE__));
